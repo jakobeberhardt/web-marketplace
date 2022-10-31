4887f7c0680cff99ed3ce75fc1753c529ce35f4d
Kevin Schwab <scke1027@h-ka.de>
2022-10-31 17:25:05 +0100

    Add fetchAllShipments

--- src/components/ShipmentTable.tsx
+++ src/components/ShipmentTable.tsx
@@ -3,19 +3,20 @@ import { getShipments } from "../service/ShipmentService";
 import Shipment from "../types/Shipment";
 
 export default function ShipmentTable() {
-    const fetchAllShipments = () => {
-        getShipments()
-          .then(shipments => {
-            console.log(shipments);
-          })
-      }
-    
-      const rows = [
-        new Shipment({id: 1, tmsReference: "Test", position: 123, label: "Test", pickupReference: "Test", deliveryReference: "Test", totalItemCount: 5, totalWeight: 0, totalVolume: 0, totalLoadMeters: 0}),
-        new Shipment({id: 2, tmsReference: "Another one", position: 17, label: "Label", pickupReference: "ExampleRef", deliveryReference: "RefExample", totalItemCount: 12, totalWeight: 99, totalVolume: 4, totalLoadMeters: 5}),
-        //new Shipment(fetchAllShipments())
-      ];
-    
+    let rows: Shipment[] = [];
+  
+    function fetchShipments() {
+      fetch('http://localhost:8080/contracts')
+        .then(res => res.json())
+        .then(data => {
+          console.log("in test: " + data)
+          rows.push(... data)
+        })
+    }
+
+      fetchShipments()
+      setTimeout(() => console.log("Timeout"), 0)
+
       return (
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -37,10 +38,10 @@ export default function ShipmentTable() {
                 >
                   <TableCell align="right">{row.id}</TableCell>
                   <TableCell align="right">{row.tmsReference}</TableCell>
-                  <TableCell align="right">{row.position.toString()}</TableCell>
+                  <TableCell align="right">Test</TableCell>
                   <TableCell align="right">{row.pickupReference}</TableCell>
                   <TableCell align="right">{row.deliveryReference}</TableCell>
-                  <TableCell align="right">{row.totalItemCount?.toString()}</TableCell>
+                  <TableCell align="right">Test</TableCell>
                 </TableRow>
               ))}
             </TableBody>
