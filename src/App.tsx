fb5cd72ccf8498ed61eee4e75880a4b5a1ece4a3
Tobias Doninger <tobias.doninger@ruffconsult.de>
2022-11-01 20:59:16 +0100

    Basic Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
 import ShipmentTable from './components/ShipmentTable';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <ShipmentTable/>
+  return <div className='wrapper'>
+    <nav>
+      <ul>
+        <li><a href="/Home">Home</a></li>
+        <li><a href='/'>Shipments</a></li>
+      </ul>
+    </nav>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ShipmentTable/>}/>
+        <Route path='/Home' element={<h1>Home</h1>}/>
+      </Routes>
+    </BrowserRouter>
+  </div>
 }
 export default App;
