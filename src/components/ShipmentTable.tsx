565fd032e4ded20f090184b809e07dc6ddad2919
Tobias Doninger <tobias.doninger@ruffconsult.de>
2022-11-03 09:16:49 +0100

    use Hook instead of useEffect

--- src/components/ShipmentTable.tsx
+++ src/components/ShipmentTable.tsx
@@ -1,5 +1,6 @@
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material'
-import { useEffect, useState } from 'react';
+//import { useEffect, useState, use } from 'react';
+import { use } from 'react';
 //import Contract from '../types/Contracts';
 
 function TableContent({items}:any) {
@@ -10,7 +11,6 @@ function TableContent({items}:any) {
           key={item.id}
           sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
         >
-          <TableCell align="right">{item.id}</TableCell>
           <TableCell align="right">{item.shipment.id}</TableCell>
           <TableCell align="right">{item.shipment.tmsReference}</TableCell>
           <TableCell align="right">{item.shipment.pickupReference}</TableCell>
@@ -22,22 +22,17 @@ function TableContent({items}:any) {
   )
 }
 
+const getItems = fetch('https://api.jeberhardt.dev/biddings').then(res => res.json());
+
 export default function ShipmentTable() {
 
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-      fetch('https://api.jeberhardt.dev/biddings')
-        .then(res => res.json())
-        .then(data => setItems(data));
-    }, []);
+    const items = use<any>(getItems);
 
     return (
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="right">ID</TableCell>
               <TableCell align="right">Shipment ID</TableCell>
               <TableCell align="right">TMSReference</TableCell>
               <TableCell align="right">PickupReference</TableCell>
