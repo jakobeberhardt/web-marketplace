57a033e2fd77e69b4941949c20e9b771ec15cdac
Tobias Doninger <tobias.doninger@ruffconsult.de>
2022-11-12 11:37:30 +0100

    User object and Post towards registration endpoint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
+
+async function register() {
+  const response: any = await fetch("http://localhost:8080/api/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "Jakob",
+      password: "123",
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  console.log(response);
+}
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
-      
-      </div>
-  )
+      <Button variant="contained" onClick={register}>
+        Register
+      </Button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/types/User.ts
@@ -0,0 +1,46 @@
+import { Collection } from "typescript";
+
+export default class User{
+    id!: String;
+    private _username!: string;
+    private _password!: string;
+
+    get username(): string {
+        return this._username
+    };
+
+    set username(username: string) {
+        this._username = username
+    }
+
+    get password(): string {
+        return this._password
+    };
+
+    set password(password: string) {
+        this._password = password
+    }
+
+    /* isAccountNonExpired() : Boolean {
+        return true
+    }
+
+    isAccountNonLocked() : Boolean {
+        return true
+    }
+
+    isCredentialsNonExpired() : Boolean {
+        return true
+    }
+
+    isEnabled() : Boolean {
+        return true
+    } */
+
+    constructor(initializer?: any){
+        if(!initializer) return;
+        if(initializer.id) this.id = initializer.id;
+        if(initializer.username) this._username = initializer.username;
+        if(initializer.password) this._password = initializer.password;
+    }
+} 
