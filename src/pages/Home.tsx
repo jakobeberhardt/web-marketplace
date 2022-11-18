4a04d9acb07f8a930002607ef52390f76da94015
Annika Kuntz <annikakuntz@web.de>
2022-11-18 09:33:38 +0100

    landingpage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,22 @@
+import { Card, Typography } from "@mui/material";
 import React from "react";
+import Lottie from "react-lottie-player";
+import truckLottie from "./../LottieGraphics/car-loading.json";
 
 function Home() {
   return (
     <>
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Card style={{ padding: "50px" }}>
+        <Typography variant="h3" component="h3">
+          Bitte loggen Sie sich ein, um Ihre Aktivitäten einsehen zu können.
+        </Typography>
+        <Lottie
+          loop
+          animationData={truckLottie}
+          play
+          style={{ width: 400, height: 400, margin: "auto" }}
+        />
+      </Card>
     </>
   );
 }
