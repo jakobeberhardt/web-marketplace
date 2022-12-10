eddeaaa54a9b7af8f4c5c85c6077cf4d98506781
Annika Kuntz <annikakuntz@web.de>
2022-12-10 12:48:50 +0100

    render fix

--- src/components/Shipment/Offer.tsx
+++ src/components/Shipment/Offer.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 import Bidding from "../../types/Bidding";
 
-export function Offer(props: { item: Bidding }) {
+export default function Offer(props: { item: Bidding }) {
   return (
     <>
       <Card sx={{ mt: "30px", mb: "30px", borderRadius: "10px" }}>
